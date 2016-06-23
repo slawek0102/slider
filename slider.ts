e496a6f10bbff80eafc59ae875f6c03503f9b97a
Slawomir <grzebyk.slawomir@gmail.com>
2016-06-23 20:10:14 +0200

    Graphic layout ready.

--- slider.ts
+++ slider.ts
@@ -1,1 +1,67 @@
-// aa
+let picturesArray:string[] = [
+    './src_files/picture1.jpg',
+    './src_files/picture2.jpg',
+    './src_files/picture3.jpg',
+    './src_files/picture4.jpg',
+    './src_files/picture5.jpg',
+    './src_files/picture6.jpg',
+];
+
+
+// Event Listeners
+
+document.getElementById('switch-left').addEventListener("click", function () {
+    slider.switchLeft()
+});
+
+document.getElementById('switch-right').addEventListener("click", function () {
+    slider.switchRight()
+});
+
+
+
+
+class Slider {
+
+    _picturesArray:string[];
+    _numberOfpictures:number;
+    _currentImgIndex:number = 0;
+
+    constructor(picturesArray) {
+        this._picturesArray = picturesArray;
+        this._numberOfpictures = this._picturesArray.length;
+    };
+
+
+    switchLeft() {
+        this._currentImgIndex = this._currentImgIndex - 1;
+        if (this._currentImgIndex < 0) {
+            this._currentImgIndex = this._picturesArray.length - 1;
+        }
+        this.displayPicture();
+    };
+
+    switchRight() {
+        this._currentImgIndex = this._currentImgIndex + 1;
+        if (this._currentImgIndex > this._picturesArray.length - 1) {
+            this._currentImgIndex = 0;
+        }
+        this.displayPicture();
+    };
+
+    private displayPicture(){
+
+       var x =document.getElementById('img');
+       x.setAttribute("src", this._picturesArray[this._currentImgIndex]);
+
+       document.appendChild(x);
+
+
+    }
+
+}
+
+
+var slider = new Slider(picturesArray);
+
+
